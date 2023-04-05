6639626ef318273547398d6a0c6d77a7f909f7d0
yana <chuprunovayana@gmail.com>
2023-04-05 08:00:33 +0200

    Initial commit

---
+++ src/main/java/com/echobot/vk_bot/VkBotApplication.java
@@ -0,0 +1,13 @@
+package com.echobot.vk_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/echobot/vk_bot/VkBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echobot.vk_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
